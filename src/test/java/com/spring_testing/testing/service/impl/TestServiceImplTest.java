ce87867e07124ff2a4da4f240405420ee18f4bd7
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2026-04-16 10:33:38 +0100

    Simple addition test

--- src/test/java/com/spring_testing/testing/service/impl/TestServiceImplTest.java
+++ src/test/java/com/spring_testing/testing/service/impl/TestServiceImplTest.java
@@ -1,10 +1,12 @@
 package com.spring_testing.testing.service.impl;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.spring_testing.testing.repository.TestRepository;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static org.assertj.core.api.Fail.fail;
 
@@ -12,22 +14,33 @@ import static org.assertj.core.api.Fail.fail;
 @DisplayName("TestServiceImpl Unit test")
 public class TestServiceImplTest {
 
+
+    @Mock
+    private TestRepository testRepository;
+
+    @InjectMocks
     private TestServiceImpl testServiceImpl; // this is the class we want to test
-
-    @Test
-    public void test() {
-        fail();
-    }
-
-    @Test
-    public void test2() {
-
-    }
 
     @Test
     @Disabled
     public void test3() {
 
+    }
+
+    @Nested
+    @DisplayName("Testing addition")
+    class TestAdditionScenarios {
+        @Test
+        void shouldTestAddition() {
+            // Given
+            final int num1 = 1;
+            final int num2 = 2;
+            // When
+            final Integer result = testServiceImpl.addition(num1, num2);
+            // Then
+            assertNotNull(result);
+            assertEquals(3, result);
+        }
     }
 }
 
